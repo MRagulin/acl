7b59bbf34e0917ed8c798c9cead844d5e7176bbb
MRagulin <mikhail.ragulin@gmail.com>
2021-03-19 12:49:47 +0300

    [F] modify wsgi

--- acladmin/wsgi.py
+++ acladmin/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for acladmin project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/var/www/acladmin')
+sys.path.append('/var/www/acladmin/acladmin')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acladmin.settings')
-
 application = get_wsgi_application()
--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 mkdir -p /var/www/ACL/log
 apt-get update
-sudo apt-get install postgresql
-sudo apt-get install python-psycopg2
 apt-get install libxslt-dev libxml2-dev libpam-dev libedit-dev
-pip install psycopg2
+apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
+sudo a2enmod wsgi
 
+#apt-get install -y libapache2-mod-wsgi-py3
+apt-get install postgresql
+apt-get install python-psycopg2
+
+pip3 install psycopg2
+pip3 install Django==3.1.6
 
